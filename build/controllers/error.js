25af2f582dcf5eabb18b63bae11fe55668dd97d2
cscristian <welcome_salta@hotmail.com>
2023-04-16 20:58:07 -0300

    Update

--- build/controllers/error.js
+++ build/controllers/error.js
@@ -1,10 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.ErrorController = void 0;
-const codes_1 = require("../http/codes");
 class ErrorController {
     static error(err, req, res, next) {
-        return res.status(codes_1.STATUS_UNAUTHORIZE).send(err);
+        return res.status(500).send("No authorizado papi");
     }
 }
 exports.ErrorController = ErrorController;
