d84fddccdec2811d3eae0360e63edd4a3d8e8996
cscristian <welcome_salta@hotmail.com>
2023-04-16 14:06:22 -0300

    Update

---
+++ build/http/codes.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.STATUS_UNAUTHORIZE = exports.STATUS_OK = void 0;
+exports.STATUS_OK = 200;
+exports.STATUS_UNAUTHORIZE = 401;
